39e81d86ab8d81a26dbbc0a061bf73a912c4f0cf
Michał Zieliński <michal@zielinscy.org.pl>
2012-02-16 11:24:42 +0100

    added gitignore

---
+++ tests/basic/dict-tests.py
@@ -0,0 +1,28 @@
+import sys
+
+try:
+    c = string_dict()
+except NameError:
+    print 'using normal dict'
+    c = {}
+
+for i in [1, 2, 3]:
+    assert i in (1,2,3)
+
+def func():
+    s = [] 
+    
+    for i in xrange(5000, 7):
+        s.append(str(i))
+    
+    s += [ str(i) for i in xrange(5000, 3) ]
+    
+    for s1 in s:
+        c[s1] = s1 + 'c'
+    
+    for s1 in s:
+        assert c[s1] == s1 + 'c'
+
+func()
+c = {}
+func()
