b31cb77c829d47d111de1c9d905ec42a52e70d71
Michał Zieliński <michal@zielinscy.org.pl>
2012-09-11 21:37:00 +0200

    fixed SString.lower/upper to compile on Java 5

--- java/pyjvm/SString.java
+++ java/pyjvm/SString.java
@@ -1,15 +1,15 @@
 // Copyright (C) 2011 by Michal Zielinski
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,41 +27,41 @@ import java.util.Arrays;
 public final class SString extends NativeObj { //!export SString BaseString
 	public final byte[] bytes;
 	public int interned = -1;
-	
+
 	public SString() {
 		this.bytes = new byte[0];
 	}
-	
+
 	public SString(byte b) {
 		this.bytes = new byte[]{ b };
 	}
-	
+
 	public SString(String data) {
 		this(data.getBytes());
 	}
-	
+
 	public SString(byte[] data) {
 		this.bytes = data;
 	}
-	
+
 	public String toString() {
 		return new String(bytes);
 	}
-	
+
 	public Obj repr_bytes() { //!export
 		SStringBuilder builder = new SStringBuilder(this.length() * 5);
-		
+
 		for(int i=0; i < length(); i++) {
 			byte ch = charAt(i);
 			builder.append(((int)ch) + ",");
 		}
-		
+
 		return builder.getValue();
 	}
-	
+
 	public Obj repr() {
 		SStringBuilder builder = new SStringBuilder(this.length() * 15 / 10);
-		
+
 		builder.append('"');
 		for(int i=0; i < length(); i++) {
 			byte ch = charAt(i);
@@ -73,18 +73,18 @@ public final class SString extends NativeObj { //!export SString BaseString
 				builder.append(ch);
 		}
 		builder.append('"');
-		
+
 		return builder.getValue();
 	}
-	
+
 	public byte charAt(int pos) {
 		return bytes[pos];
 	}
-	
+
 	public void copyTo(byte[] dest, int offset) {
 		System.arraycopy(this.bytes, 0, dest, offset, this.length());
 	}
-	
+
 	public int length() {
 		return bytes.length;
 	}
@@ -92,49 +92,49 @@ public final class SString extends NativeObj { //!export SString BaseString
 	public int hashCode() {
 		//if(this.interned != -1) // UAHAHAHAHAHAHA
 			//return this.interned;
-		
+
 		int h = 0;
 		byte val[] = bytes;
 		int len = bytes.length;
-		
+
 		for (int i = 0; i < len; i++) {
 		    h = 31*h + val[i];
 		}
 		return h;
 	}
-	
+
 	public SString add(SString other) {
 		byte[] newBytes = new byte[this.length() + other.length()];
 		this.copyTo(newBytes, 0);
 		other.copyTo(newBytes, this.length());
 		return new SString(newBytes);
 	}
-	
+
 	public Obj add(Obj other) {
 		if(other instanceof SString)
 			return this.add((SString)other);
 		return NotImplemented;
 	}
-	
+
 	public SString mul(int times) {
 		SStringBuilder builder = new SStringBuilder(times * length());
 		for(int i=0; i<times; i++)
 			builder.append(this);
 		return builder.getValue();
 	}
-	
+
 	public Obj mul(Obj other) {
 		if(other instanceof SInt)
 			return this.mul(other.intValue());
 		return NotImplemented;
 	}
-        
-	public SString join(Obj seq) { //!export 
+
+	public SString join(Obj seq) { //!export
 		Obj iter = seq.getIter();
 		SStringBuilder builder = new SStringBuilder(16);
 		boolean first = true;
 		Obj item;
-		
+
 		while((item=iter.next()) != null) {
 			if(!first) {
 				builder.append(this);
@@ -143,14 +143,14 @@ public final class SString extends NativeObj { //!export SString BaseString
 			}
 			builder.append(item.stringValue());
 		}
-		
+
 		return builder.getValue();
 	}
-	
+
 	public static Obj construct(Obj val) { //!export - <new>
 		return val.str();
 	}
-	
+
 	public SBool isEqual(Obj other) {
 		if(other == this)
 			return SBool.True;
@@ -171,7 +171,7 @@ public final class SString extends NativeObj { //!export SString BaseString
 			return null;
 		}
 	}
-	
+
 	public SString strip() { //!export
 		int left, right;
 		for(left=0; left < length(); left++) {
@@ -186,25 +186,31 @@ public final class SString extends NativeObj { //!export SString BaseString
 		}
 		return (SString)getSlice(SInt.get(left), SInt.get(right));
 	}
-	
+
 	public SString lower() { //!export
-		SString n = new SString(Arrays.copyOf(bytes, bytes.length));
+		SString n = dataCopy();
 		for(int i=0; i<bytes.length; i++)
 			n.bytes[i] = (byte) Character.toLowerCase(n.bytes[i]);
 		return n;
 	}
-	
+
 	public SString upper() { //!export
-		SString n = new SString(Arrays.copyOf(bytes, bytes.length));
+		SString n = dataCopy();
 		for(int i=0; i<bytes.length; i++)
 			n.bytes[i] = (byte) Character.toUpperCase(n.bytes[i]);
 		return n;
 	}
-	
+
 	public boolean contains(Obj other) { //!export
 		return find(other, 0) != -1;
 	}
-	
+
+	private SString dataCopy() {
+		byte[] newBytes = new byte[bytes.length];
+		System.arraycopy(bytes, 0, newBytes, 0, bytes.length);
+		return new SString(newBytes);
+	}
+
 	public int find(Obj[] args) { //!export direct
 		if(args.length > 2 || args.length == 0)
 			throw new ScriptError(ScriptError.TypeError, "str.find() requires 1 or 2 arguments");
@@ -217,7 +223,7 @@ public final class SString extends NativeObj { //!export SString BaseString
 		}
 		return find(other, start);
 	}
-	
+
 	public int find(Obj other, int start) {
 		SString o = other.stringValue();
 		byte[] ob = o.bytes;
@@ -237,9 +243,9 @@ public final class SString extends NativeObj { //!export SString BaseString
 			// I know that it is useless
 			// but tommorow I have contest
 			int[] pi = new int[ob.length];
-			
+
 			// compute Knuth prefix function
-			
+
 			int q = 0;
 			pi[0] = 0;
 			for(int i=1; i<pi.length; ) {
@@ -253,7 +259,7 @@ public final class SString extends NativeObj { //!export SString BaseString
 				pi[i] = q;
 				i++;
 			}
-			
+
 			// find
 			q = 0;
 			for(int i=start; i<bytes.length; ) {
@@ -273,11 +279,11 @@ public final class SString extends NativeObj { //!export SString BaseString
 		}
 		return -1;
 	}
-	
+
 	public SString stringValue() {
 		return this;
 	}
-	
+
 	public static SString fromJavaString(String string) {
 		try {
 			return new SString(string.getBytes("utf8"));
@@ -285,20 +291,20 @@ public final class SString extends NativeObj { //!export SString BaseString
 			throw new ScriptError(ScriptError.LookupError, ex);
 		}
 	}
-	
+
 	public Type getType() {
 		return SStringClass.instance;
 	}
-	
-	
+
+
 	public Obj add(String string) {
 		return add(new SString(string));
 	}
-	
+
 	public boolean boolValue() {
 		return bytes.length != 0;
 	}
-	
+
 	public Obj getSlice(Obj lower, Obj upper) {
 		int len = this.length();
 		int l = (lower == None)? 0 : lower.intValue();
@@ -315,12 +321,12 @@ public final class SString extends NativeObj { //!export SString BaseString
 		} else if(u >= len) {
 			u = len;
 		}
-		
+
 		byte[] n = new byte[u - l];
 		System.arraycopy(this.bytes, l, n, 0, u - l);
 		return new SString(n);
 	}
-	
+
 	public Obj getItem(int index) {
 		int nindex = index;
 		if(index < 0)
@@ -329,11 +335,11 @@ public final class SString extends NativeObj { //!export SString BaseString
 			throw new ScriptError(ScriptError.IndexError, "bad index " + index);
 		return new SString(bytes[nindex]);
 	}
-	
+
 	public Obj getItem(Obj key) {
 		return getItem(key.intValue());
 	}
-	
+
 	public Obj compare(Obj other) {
 		SString o = (SString)other;
 		int min = Math.min(o.bytes.length, bytes.length);
@@ -344,12 +350,12 @@ public final class SString extends NativeObj { //!export SString BaseString
 		}
 		if(o.bytes.length == bytes.length)
 			return SInt.ZERO;
-		
+
 		return o.bytes.length < bytes.length ? SInt.ONE: SInt.MINUS_ONE;
 	}
-	
+
 	// intern
-	
+
 	public final int intern() {
 		if(this.interned != -1)
 			return this.interned;
@@ -361,13 +367,13 @@ public final class SString extends NativeObj { //!export SString BaseString
 				reverseInternTable.put(SInt.get(key), this);
 				this.interned = key;
 			} else {
-				this.interned = val.intValue(); 
+				this.interned = val.intValue();
 			}
-			
+
 			return this.interned;
 		}
 	}
-	
+
 	public static final SString unintern(int code) {
 		SString result;
 		synchronized (internTable) {
@@ -377,7 +383,7 @@ public final class SString extends NativeObj { //!export SString BaseString
 			throw new ScriptError(ScriptError.LookupError, code + " is not a key of an interned string");
 		return result;
 	}
-	
+
 	public static final Dict internTable = new Dict(128);
 	public static final Dict reverseInternTable = new Dict(128);
 	private static int nextIdent = 0;
